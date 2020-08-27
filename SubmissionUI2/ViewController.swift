4c8dac0e476391dfcae648f1023372b8ea08f2c0
Tushar <tushare0509@gmail.com>
2020-08-27 19:38:28 +0530

    Initial Commit

---
+++ SubmissionUI2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubmissionUI2
+//
+//  Created by Tushar Elangovan on 8/27/20.
+//  Copyright © 2020 Tushar Elangovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
